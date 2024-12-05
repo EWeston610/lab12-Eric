4fcbca13c9ffbff6b206a4fd184c41db192356c0
Eric Weston <eric.weston@students.snow.edu>
2024-12-05 15:59:16 -0700

    Step A done and commited

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,36 +1,48 @@
-//Eric Weston and Garin Humpherys
-//Lab 1 8/29/24
-
-using System.Data;
-
 System.Console.WriteLine("automated testing ...");
-System.Diagnostics.Debug.Assert(ShiftedCharacter('a') == 'b');
-System.Diagnostics.Debug.Assert(ShiftedCharacter('z') == 'a');
-System.Diagnostics.Debug.Assert(LetterPosition('a') == 0);
-System.Diagnostics.Debug.Assert(LetterPosition('b') == 1);
-System.Diagnostics.Debug.Assert(LetterPosition('z') == 25);
-System.Diagnostics.Debug.Assert(WrapLetterPosition(0) == 0);
-System.Diagnostics.Debug.Assert(WrapLetterPosition(3) == 3);
-System.Diagnostics.Debug.Assert(WrapLetterPosition(26) == 0);
-System.Diagnostics.Debug.Assert(WrapLetterPosition(29) == 3);
+System.Diagnostics.Debug.Assert(Library.ShiftedCharacter('a') == 'b');
+System.Diagnostics.Debug.Assert(Library.ShiftedCharacter('z') == 'a');
+System.Diagnostics.Debug.Assert(Library.LetterPosition('a') == 0);
+System.Diagnostics.Debug.Assert(Library.LetterPosition('b') == 1);
+System.Diagnostics.Debug.Assert(Library.LetterPosition('z') == 25);
+System.Diagnostics.Debug.Assert(Library.WrapLetterPosition(0) == 0);
+System.Diagnostics.Debug.Assert(Library.WrapLetterPosition(3) == 3);
+System.Diagnostics.Debug.Assert(Library.WrapLetterPosition(26) == 0);
+System.Diagnostics.Debug.Assert(Library.WrapLetterPosition(29) == 3);
 System.Console.WriteLine("tests passed.");
 
-char ShiftedCharacter(char originalCharacter) {
-    return CharacterFromAscii(WrapLetterPosition (LetterPosition(originalCharacter) + 1) + 'a');
-}
-
-char CharacterFromAscii(int asciiValue) {
-    return (char) asciiValue;
-}
-
-int LetterPosition(char originalCharacter) {
-    return (originalCharacter - 'a');
-
-}
-
-int WrapLetterPosition(int Position) {
-    return (Position % 26);
-}
-
 Console.Write("Please Enter a character (I will display the shifted character): ");
-Console.WriteLine(ShiftedCharacter(Console.ReadKey(true).KeyChar));
+while(true){
+    Console.Write(Library.ShiftedCharacter(Console.ReadKey(true).KeyChar));
+}
+public class Library
+{
+    public static char ShiftedCharacter(char originalCharacter)
+    {
+        
+        return CharacterFromAscii(Library.WrapLetterPosition(LetterPosition(originalCharacter) + 1) + 'a');
+    }
+
+    public static char CharacterFromAscii(int asciiValue)
+    {
+        return (char)asciiValue;
+    }
+
+    public static int LetterPosition(char originalCharacter)
+    {
+        return (originalCharacter - 'a');
+
+    }
+
+    public static int WrapLetterPosition(int Position)
+    {
+        return (Position % 26);
+    }
+}
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
